8ed20c0f19da6cad06ff5762b3d7b8a577aaa28c
何凌锋 <2017201524@ruc.edu.cn>
2022-04-29 19:52:13 +0800

    添加项目文件。

---
+++ Hedaozi.DataCollector/DataModels/BaiduIndex.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hedaozi.DataCollector.DataModels
+{
+    public partial class BaiduIndex
+    {
+        private List<string> keywords = new List<string>();
+        private List<int> area = new List<int>();
+        private string startDate = null;
+        private string endDate = null;
+        private string cookies = null;
+
+        public BaiduIndex()
+        {
+
+        }
+    }
+
+    public partial class BaiduIndex
+    {
+        public enum IndexType { Search, News, Feed }
+
+        public enum PlotformType { All, PC, Wise }
+
+        public enum AreaType { All, Province, Prefectures }
+
+        public class Area
+        {
+            public string BdIndexCode { get; set; }
+            public string Name { get; set; }
+            public string FullName { get; set; }
+            public string NbscCode { get; set; }
+            public AreaType Type { get; set; }
+            public string Belong { get; set; }
+            public bool Deprecated { get; set; }
+        }
+
+        public static class Areas
+        {
+            static System.Xml.XmlDocument areasTree;
+
+            static Areas()
+            {
+                Uri codeFilePath = new Uri("Codebook/BdIndexCode.xml");
+                areasTree = General.XmlHelper.LoadXml(codeFilePath);
+            }
+
+            public static List<Area> FindAreas(string filter)
+            {
+                return new List<Area>();
+            }
+
+            public static string QueryFilter(AreaType type = AreaType.All, string belong = null)
+            {
+
+                return "";
+            }
+        }
+    }
+}
---
+++ Hedaozi.DataCollector/General/XmlHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Hedaozi.DataCollector.General
+{
+    internal static class XmlHelper
+    {
+        public static XmlDocument LoadXml(Uri path)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(path.ToString());
+            return document;
+        }
+    }
+}
